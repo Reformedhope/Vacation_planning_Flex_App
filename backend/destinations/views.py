98720c02151b7e92a114478068909b53dd9256a4
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-05-20 20:00:04 -0400

    created a URLs file and updated the list views function

--- backend/destinations/views.py
+++ backend/destinations/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import DestinationSerializer
+from .models import Destinations
+
 
 # Create your views here.
--- backend/lists/views.py
+++ backend/lists/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework.permissions import  IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ListSerializer
+from .models import List
+
 
 # Create your views here.
+
+#Protected/Actual needed function.
+@api_view(['GET', 'POST',])
+@permission_classes([IsAuthenticated]) # the personmissions class ties into Django's standard django.contrib.auth model permissions. This permission must only be applied to views that have a .queryset property or get_queryset () method. Authorization will only be granted if the user is authenticated and has the relevant model permissions assigned.
+#We are able to use with wihen AllowAnyis in the Params in list ([]).As Well as IsAuthenticated. 
+def get_travelers_items(request, list_id): #What goes into the parameters can be used for the function.
+    print(
+        'User', f"{request.user.firt_name}{request.user.last_name}")
+    if request.method =='GET':
+        travelers_list = List.object.filter(list_id=list_id)
+        serializer = ListSerializer(travelers_list, many = True)
+        return Response(serializer.data)
+    if request.method =='POST':
+        serializer = ListSerializer( data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, list_id=list_id)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+    
+
+
+       
+        
