927e68f40b463d5eb5c1edd13dd650d0250f3a01
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-05-28 20:21:59 -0400

    created list page and get request function to pull list, created post request and add an item function

--- frontend/src/components/AddAItem/AddAItem.jsx
+++ frontend/src/components/AddAItem/AddAItem.jsx
@@ -1,5 +1,67 @@
+import React from "react";
+import { useEffect,useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+
+
+
 const AddAitem = (props) => {
-    return (  );
+     const[user, token]= useAuth();
+     const[item, setItem]= useState([]);
+     const[itemsNeeeded, setItemsNeeded] =useState(0);
+    
+    
+    
+    
+    function handleSubmit(event){    
+        // event.preventDefault();
+        let newItem = {
+            items: item,
+            items_needed:itemsNeeeded,
+        };
+        createAItem(newItem)
+    }
+
+    async function createAItem(newItem){
+        try{
+        let response = await axios.post ('http://127.0.0.1:8000/api/vacation/lists/list/',newItem, {
+            headers:{
+                Authorization:'Bearer ' + token
+              }
+
+        })
+    }catch(error){
+        console.log(error.message)
+    }
+
+    }
+    
+    
+    
+    return (
+    
+        <div>
+        <h2>{user.username}</h2> 
+        <form onSubmit={handleSubmit}>
+            <input type='text' value={item} onChange={(event) => setItem(event.target.value)}/>
+            <input type='number' value={itemsNeeeded} onChange={(event) => setItemsNeeded(event.target.value)}/>
+            <button type='submit'>Add Another Item</button>
+        </form>
+        </div>
+           
+
+
+
+
+
+
+
+
+
+
+
+
+     );
 }
  
 export default AddAitem;
--- frontend/src/pages/ListPage/ListPage.jsx
+++ frontend/src/pages/ListPage/ListPage.jsx
@@ -1,46 +1,57 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import useAuth from "../../hooks/useAuth";
-
+import AddAitem from "../../components/AddAItem/AddAItem";
 
 const ListPage = (props) => {
-    const [user, token] = useAuth();
-    const [items, setItems] = useState([]);
-    const [itemsNeeded, setItemsNeeded] = useState(0); 
-    console.log(user)
-    console.log(token)
+  const [user, token] = useAuth();
+  const [items, setItems] = useState([]);
+  const [itemsNeeded, setItemsNeeded] = useState(0);
+  console.log(user);
+  console.log(token);
 
-   
-        useEffect(() => {
-            const fetchItems = async () => {
-              try {
-                let response = await axios.get("http://127.0.0.1:8000/api/vacation/lists/list/", {
-                  headers: {
-                    Authorization: "Bearer " + token,
-                  },
-                });
-                setItems(response.data);
-              } catch (error) {
-                console.log(error.response.data);
-              }
-            };
-            fetchItems();
-          }, [token]);
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        let response = await axios.get(
+          "http://127.0.0.1:8000/api/vacation/lists/list/",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        setItems(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    fetchItems();
+  }, [token]);
 
-    
-    return ( 
-        <ol>
-            {items && items.map((item) => (
-          <p key={item.id}>
-            <li>{item.items} {item.items_needed}</li>
-          </p>
-        ))}
-        </ol>
+  return (
+    <div>
+        <AddAitem fetchItems = {items}/>   
+      <ol>
+        {items &&
+          items.map((item) => (
+            <p key={item.id}>
+              <li>
+                {item.items} {item.items_needed}
+              </li>
+            </p>
+          ))}
+      </ol>
+      <ol>
+      <li>Phone Charger</li>
+      <input type="checkbox"/>
+    <li> Extra Shoes</li>
+              <li>Comfy Slipper or socks</li>
+              <li>Pillow</li>
+              <li>Loofa or Washcloth</li>
+      </ol>
+    </div>
+  );
+};
 
-
-
-
-    );
-}
- 
 export default ListPage;
